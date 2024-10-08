8b2d915b4bc3adef9bd8e426e00c6e187b5b676c
никита зайцев <nikitakryt83@bk.ru>
2024-10-08 20:16:49 +0700

    modified:   Task 2/index.htm 	new file:   Task 2/script.js 	new file:   Task 2/style.css

---
+++ Task 2/script.js
@@ -0,0 +1,12 @@
+// Инициализация Swiper
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
